221c39cf843f14fcb1b62735eb9d71d7ef880ffe
Jerrica Bobadilla <Jerrica@Jerricas-MacBook-Pro.local>
2017-03-03 19:07:35 -0800

    created cards using javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,8 +5,24 @@ var cardThree = "king";
 var cardFour = "king";
 
 // Checking equality between two cards
-if (cardTwo === cardFour) {
+/* if (cardTwo === cardFour) {
 	alert('You found a match!')
 } else {
 	alert('Sorry, try again.')
+} */
+
+var board = document.getElementById('game-board');
+
+
+// Finding the game board and setting to a variable
+var gameBoard = document.getElementById('game-board');
+
+// Creating the cards with javascript
+var createCards = function(cards){
+	// For loop to create the cards and append to board
+	for (var i=0; i<cards; i++){
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		gameBoard.appendChild(cardElement);
+	}
 }
